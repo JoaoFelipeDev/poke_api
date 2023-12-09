c03da5d6c18e325352928c2783e091370a50beea
João Felipe <joaofelipedev01@gmail.com>
2023-12-09 14:10:11 -0300

    problema de cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
 import "express-async-errors";
-import  {routes} from '../src/routes'
+import { routes } from '../src/routes'
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
 app.use(routes);
 app.listen(3000, () => console.log('Server is running'));
